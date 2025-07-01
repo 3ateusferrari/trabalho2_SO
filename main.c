7ea34ccfb541e4a5e0b6db1f11d762a048d47d09
3ateusferrari <mateusspier@gmail.com>
2025-07-01 16:56:47 -0300

    fix: valor só pode ser potencia de 2

--- main.c
+++ main.c
@@ -2,15 +2,35 @@
 #include <stdlib.h>
 #include "memoria.h"
 #include "processo.h"
+ 
+// Função auxiliar para verificar se um número é potência de dois
+int eh_potencia_de_dois(int x) {
+    return x > 0 && (x & (x - 1)) == 0;
+}
 
 int main() {
     int tam_mem_fisica, tam_pagina, tam_max_processo;
-    printf("Tamanho da memória física (bytes): ");
-    scanf("%d", &tam_mem_fisica);
-    printf("Tamanho da página/quadro (bytes): ");
-    scanf("%d", &tam_pagina);
-    printf("Tamanho máximo de um processo (bytes): ");
-    scanf("%d", &tam_max_processo);
+    do {
+        printf("Tamanho da memória física (bytes): ");
+        scanf("%d", &tam_mem_fisica);
+        if (!eh_potencia_de_dois(tam_mem_fisica)) {
+            printf("O tamanho da memória física deve ser uma potência de dois!\n");
+        }
+    } while (!eh_potencia_de_dois(tam_mem_fisica));
+    do {
+        printf("Tamanho da página/quadro (bytes): ");
+        scanf("%d", &tam_pagina);
+        if (!eh_potencia_de_dois(tam_pagina)) {
+            printf("O tamanho da página deve ser uma potência de dois!\n");
+        }
+    } while (!eh_potencia_de_dois(tam_pagina));
+    do {
+        printf("Tamanho máximo de um processo (bytes): ");
+        scanf("%d", &tam_max_processo);
+        if (!eh_potencia_de_dois(tam_max_processo)) {
+            printf("O tamanho máximo do processo deve ser uma potência de dois!\n");
+        }
+    } while (!eh_potencia_de_dois(tam_max_processo));
 
     inicializar_memoria(tam_mem_fisica, tam_pagina);
     inicializar_processos(tam_max_processo, tam_pagina);
